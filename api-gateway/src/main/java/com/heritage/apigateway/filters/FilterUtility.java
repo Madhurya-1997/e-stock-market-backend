2ebdab7513b98364e32ec91aa64784176d0b9910
Madhurya Bharadwaaz <dg.hipl@gmail.com>
2022-06-23 09:48:46 +0530

    added FilterUtiliy

--- api-gateway/src/main/java/com/heritage/apigateway/filters/FilterUtility.java
+++ api-gateway/src/main/java/com/heritage/apigateway/filters/FilterUtility.java
@@ -1,4 +1,31 @@
 package com.heritage.apigateway.filters;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.net.http.HttpHeaders;
+import java.util.List;
+
+@Component
 public class FilterUtility {
+
+    public static final String CORRELATION_ID = "e-stock-market-correlation-id";
+
+    public String getCorrelationId(HttpHeaders requestHeaders) {
+        if (requestHeaders.get(CORRELATION_ID) != null) {
+            List<String> requestHeaderList = requestHeaders.get(CORRELATION_ID);
+            return requestHeaderList.stream().findFirst().get();
+        } else {
+            return null;
+        }
+    }
+
+    public ServerWebExchange setRequestHeader(ServerWebExchange exchange, String name, String value) {
+        return exchange.mutate().request(exchange.getRequest().mutate().header(name, value).build()).build();
+    }
+
+    public ServerWebExchange setCorrelationId(ServerWebExchange exchange, String correlationId) {
+        return this.setRequestHeader(exchange, CORRELATION_ID, correlationId);
+    }
+
 }
